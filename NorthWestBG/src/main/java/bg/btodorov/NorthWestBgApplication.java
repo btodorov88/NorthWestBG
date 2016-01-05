483480cd94941d1a27d1c0ec09b0983d34a0c45a
btodorov <btodorov@vmware.com>
2016-01-05 15:52:45 +0200

    Initial commit

---
+++ NorthWestBG/src/main/java/bg/btodorov/NorthWestBgApplication.java
@@ -0,0 +1,12 @@
+package bg.btodorov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthWestBgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthWestBgApplication.class, args);
+	}
+}
