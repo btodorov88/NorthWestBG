330b9265f94f0d33d6b44b5e97bc5db8c92af620
btodorov <btodorov@vmware.com>
2016-02-22 11:25:11 +0200

    getPlaceById added to the controller

--- NorthWestBG/src/main/java/bg/btodorov/controller/PlaceController.java
+++ NorthWestBG/src/main/java/bg/btodorov/controller/PlaceController.java
@@ -4,10 +4,7 @@ import bg.btodorov.model.Place;
 import bg.btodorov.repo.PlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,12 @@ public class PlaceController {
     @ResponseBody
     public List<Place> getAllPlaces(){
         return repository.findAll();
+    }
+
+    @RequestMapping("/api/getPlaceById")
+    @ResponseBody
+    public Place getPlaceById(@RequestParam("id") String id){
+        return repository.findOne(id);
     }
 
     @RequestMapping(value = "/api/addPlace", method = RequestMethod.POST)
